59954da9362894baea7bacf5b3a8794614bb39dc
user <user@example.com>
2024-02-25 12:08:57 +0200

    functions realization

--- src/db_manager.py
+++ src/db_manager.py
@@ -1,5 +1,6 @@
 import psycopg2
 import requests
+from functools import wraps
 
 
 class DBManager:
@@ -20,7 +21,7 @@ class DBManager:
         self.id = the_biggest_employer['id']
         self.url = the_biggest_employer['url']
         self.vacancies_url = the_biggest_employer['vacancies_url']
-        self.vacancies = requests.get(url=self.vacancies_url).json()['items']
+        self.vacancies = requests.get(url=f"{self.vacancies_url}&per_page=100").json()['items']
 
         # начинаем работу с БД
         conn = psycopg2.connect(
@@ -33,24 +34,27 @@ class DBManager:
         try:
             with conn:
                 with conn.cursor() as cur:
-                    # проверяем таблицы БД hh_vacancies на существование
-                    cur.execute("""CREATE TABLE IF NOT EXISTS employers(
-                                                    id int PRIMARY KEY,
-                                                    employer_name varchar,
-                                                    url varchar,
-                                                    vacancies_url varchar
-                                                )""")
-                    cur.execute("""CREATE TABLE IF NOT EXISTS vacancies(
-                                                    id int PRIMARY KEY,
-                                                    vacancy_name varchar,
-                                                    url varchar,
-                                                    salary_from int,
-                                                    salary_to int,
-                                                    employer_id int,
+
+                    # создаём таблицы employers и vacancies, если они не существуют
+                    cur.execute("""
+                                CREATE TABLE IF NOT EXISTS employers(
+                                    id int PRIMARY KEY,
+                                    employer_name varchar,
+                                    url varchar,
+                                    vacancies_url varchar
+                                )""")
+                    cur.execute("""
+                                CREATE TABLE IF NOT EXISTS vacancies(
+                                    id int PRIMARY KEY,
+                                    vacancy_name varchar,
+                                    url varchar,
+                                    salary_from int,
+                                    salary_to int,
+                                    employer_id int,
                                                     
-                                                    CONSTRAINT vacancy_employer_id FOREIGN KEY (employer_id) 
-                                                    REFERENCES employers(id)
-                                                )""")
+                                    CONSTRAINT vacancy_employer_id FOREIGN KEY (employer_id) 
+                                    REFERENCES employers(id)
+                                )""")
 
                     # добавляем данные о работодателе и вакансиях в таблицы
                     try:
@@ -73,77 +77,77 @@ class DBManager:
         finally:
             conn.close()
 
-    def get_companies_and_vacancies_count(self):
-        conn = psycopg2.connect(
-            host='localhost',
-            database='hh_vacancies',
-            user='postgres',
-            password='mihanik95'
-        )
+    @staticmethod
+    def db_working(f):
+        """
+        декоратор для работы функций с БД
+        """
 
-        try:
-            with conn:
-                with conn.cursor() as cur:
-                    pass
-        finally:
-            conn.close()
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            conn = psycopg2.connect(
+                host='localhost',
+                database='hh_vacancies',
+                user='postgres',
+                password='mihanik95'
+            )
 
-    def get_all_vacancies(self):
-        conn = psycopg2.connect(
-            host='localhost',
-            database='hh_vacancies',
-            user='postgres',
-            password='mihanik95'
-        )
+            try:
+                with conn:
+                    with conn.cursor() as cur:
+                        return f(cur=cur, *args, **kwargs)
+            finally:
+                conn.close()
 
-        try:
-            with conn:
-                with conn.cursor() as cur:
-                    pass
-        finally:
-            conn.close()
+        return wrapper
 
-    def get_avg_salary(self):
-        conn = psycopg2.connect(
-            host='localhost',
-            database='hh_vacancies',
-            user='postgres',
-            password='mihanik95'
-        )
+    @staticmethod
+    @db_working
+    def get_companies_and_vacancies_count(cur):
+        """
+        счетает и возвращает количество всех компаний и их вакансий в БД
+        """
+        cur.execute("SELECT COUNT(id) FROM employers")
+        result = cur.fetchall()[0][0]
+        cur.execute("SELECT COUNT(id) FROM vacancies")
+        result += cur.fetchall()[0][0]
+        return result
 
-        try:
-            with conn:
-                with conn.cursor() as cur:
-                    pass
-        finally:
-            conn.close()
+    @db_working
+    def get_all_vacancies(self, cur):
+        """
+        возвращает все вакансии данного работодателя
+        """
+        cur.execute(f"""SELECT * FROM vacancies
+                    WHERE employer_id={self.id}""")
+        return cur.fetchall()
 
-    def get_vacancies_with_higher_salary(self):
-        conn = psycopg2.connect(
-            host='localhost',
-            database='hh_vacancies',
-            user='postgres',
-            password='mihanik95'
-        )
+    @db_working
+    def get_avg_salary(self, cur):
+        """
+        считает и возвращает среднюю зарплату по вакансиям данного работодателя
+        """
+        cur.execute(f"""SELECT AVG((salary_to+salary_from)/2)
+                    FROM vacancies
+                    WHERE employer_id={self.id}""")
+        return int(cur.fetchall()[0][0])
 
-        try:
-            with conn:
-                with conn.cursor() as cur:
-                    pass
-        finally:
-            conn.close()
+    @db_working
+    def get_vacancies_with_higher_minimal_salary(self, cur):
+        """
+        возвращает вакансии с минимальной зарплатой выше средней среди всех вакансий
+        данного работодателя
+        """
+        cur.execute(f"""
+                    SELECT * FROM vacancies
+                    WHERE salary_from>=(SELECT AVG(salary_from) FROM vacancies 
+                    WHERE employer_id={self.id})
+                    AND employer_id={self.id}
+                    """)
+        return cur.fetchall()
 
-    def get_vacancies_with_keyword(self, keyword):
-        conn = psycopg2.connect(
-            host='localhost',
-            database='hh_vacancies',
-            user='postgres',
-            password='mihanik95'
-        )
-
-        try:
-            with conn:
-                with conn.cursor() as cur:
-                    pass
-        finally:
-            conn.close()
+    @db_working
+    def get_vacancies_with_keyword(self, keyword, cur):
+        cur.execute(f"""SELECT * FROM vacancies
+                    WHERE vacancy_name LIKE '%{keyword}%'""")
+        return cur.fetchall()
